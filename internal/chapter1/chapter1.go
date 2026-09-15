ae8e4223befad682a39563b3c1f6e508f7417f80
ax_hirayama <akira_hirayama@axcel-mode.co.jp>
2026-09-15 15:32:19 +0900

    Complete chapter1 and chapter2 GET exercise

--- internal/chapter1/chapter1.go
+++ internal/chapter1/chapter1.go
@@ -1,17 +1,31 @@
 package chapter1
 
-import "net/http"
-
-// revive:disable
+import (
+	"fmt"
+	"net/http"
+)
 
 func GetEcho(w http.ResponseWriter, r *http.Request) {
-	//FIXME: Getメソッドのアクセスか確認
+	// FIXME: Getメソッドのアクセスか確認
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
-	//FIXME: パラメータをFormに変換する
+	// FIXME: パラメータをFormに変換する
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	//FIXME: パラメータを取得する
+	// FIXME: パラメータを取得する
+	form := r.Form
 
-	//FIXME: パラメータをレスポンスに書き出す
+	// FIXME: レスポンスコード設定
+	w.WriteHeader(http.StatusOK)
 
-	//FIXME: レスポンスコード設定
+	// FIXME: パラメータをレスポンスに書き出す
+	if _, err := fmt.Fprint(w, form); err != nil {
+		return
+	}
 }
--- internal/chapter2/chapter2.go
+++ internal/chapter2/chapter2.go
@@ -1,8 +1,79 @@
 package chapter2
 
-import "net/http"
-
-// revive:disable
+import (
+	"bytes"
+	"io"
+	"net/http"
+)
 
 func Get(w http.ResponseWriter, r *http.Request) {
+	req, err := http.NewRequest(
+		http.MethodGet,
+		"http://mock-api/users?age=25",
+		nil,
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("key", "dip")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	if _, err := w.Write(body); err != nil {
+		return
+	}
+}
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	body := []byte(`{
+		"name": "dip 次郎",
+		"age": 24
+	}`)
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		"http://mock-api/users",
+		bytes.NewBuffer(body),
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("key", "dip")
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	if _, err := w.Write(responseBody); err != nil {
+		return
+	}
 }
